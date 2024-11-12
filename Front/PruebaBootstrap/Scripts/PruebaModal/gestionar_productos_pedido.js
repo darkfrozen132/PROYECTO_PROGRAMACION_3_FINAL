75a5deac81df6bb4d4c817ba152f76fce79620d8
darkfrozen132 <darkfrozen@outlook.com>
2024-11-12 17:03:42 -0500

    fghasd

--- Front/PruebaBootstrap/Scripts/PruebaModal/gestionar_productos_pedido.js
+++
@@ -1,9 +0,0 @@
-﻿function showModalFormClientePedido() {
-    var modalForm = new bootstrap.Modal(document.getElementById('form-modal-pedido'));
-    modalForm.toggle();
-}
-
-function showModalFormAgregarProducto() {
-    var modalForm = new bootstrap.Modal(document.getElementById('form-modal-agregar_producto'));
-    modalForm.toggle();
-}
